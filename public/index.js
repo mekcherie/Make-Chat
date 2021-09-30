43e4161d0cf400f8b59847c3e7b561ace68e0cfa
Meklit Cherie <makywonde347@yahoo.com>
2021-09-29 18:03:35 -0700

    users can join and leave chats

--- public/index.js
+++ public/index.js
@@ -4,6 +4,8 @@ $(document).ready(()=>{
   
     //Keep track of the current user
     let currentUser;
+    // Get the online users from the server
+    socket.emit('get online users');
   
     $('#create-user-btn').click((e)=>{
       e.preventDefault();
@@ -30,10 +32,19 @@ $(document).ready(()=>{
         $('#chat-input').val("");
       }
     });
-  
-    //socket listeners
-    socket.on('new user', (username) => {
-      console.log(`${username} has joined the chat`);
-      $('.users-online').append(`<div class="user-online">${username}</div>`);
+    // socket listeners
+    //Refresh the online user list
+    socket.on('user has left', (onlineUsers) => {
+      $('.users-online').empty();
+      for(username in onlineUsers){
+        $('.users-online').append(`<p>${username}</p>`);
+      }
+});
+    socket.on('get online users', (onlineUsers) => {
+        //You may have not have seen this for loop before. It's syntax is for(key in obj)
+        //Our usernames are keys in the object of onlineUsers.
+        for(username in onlineUsers){
+          $('.users-online').append(`<div class="user-online">${username}</div>`);
+        }
     })
   })
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,24 +1,20 @@
 //chat.js
-module.exports = (io, socket) => {
+module.exports = (io, socket, onlineUsers) => {
 
     socket.on('new user', (username) => {
+      //Save the username as key to access the user's socket id
+      onlineUsers[username] = socket.id;
+      //Save the username to socket as well. This is important for later.
+      socket["username"] = username;
       console.log(`✋ ${username} has joined the chat! ✋`);
       io.emit("new user", username);
     })
   
-    //Listen for new messages
-    socket.on('new message', (data) => {
-      // Send that data back to ALL clients
-      console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
-      io.emit('new message', data);
-    })
-    //Output the new message
-    socket.on('new message', (data) => {
-        ('.message-container').append(`
-        <div class="message">
-            <p class="message-user">${data.sender}: </p>
-            <p class="message-text">${data.message}</p>
-        </div>
-        `);
-  })
-  }
+      // This fires when a user closes out of the application
+      // socket.on("disconnect") is a special listener that fires when a user exits out of the application.
+      socket.on('disconnect', () => {
+        //This deletes the user by using the username we saved to the socket
+        delete onlineUsers[socket.username]
+        io.emit('user has left', onlineUsers);
+      });
+    }
