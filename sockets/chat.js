96dc5bee650c9c390bb8a3acae9f2032725f4d02
Meklit Cherie <makywonde347@yahoo.com>
2021-09-29 17:24:48 -0700

    users can join and send messages

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,6 +1,24 @@
-/// chat.js
-socket.on('new user', (username) => {
-    console.log(`✋ ${username} has joined the chat! ✋`);
-    //Send the username to all clients currently connected
-    io.emit("new user", username);
+//chat.js
+module.exports = (io, socket) => {
+
+    socket.on('new user', (username) => {
+      console.log(`✋ ${username} has joined the chat! ✋`);
+      io.emit("new user", username);
+    })
+  
+    //Listen for new messages
+    socket.on('new message', (data) => {
+      // Send that data back to ALL clients
+      console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+      io.emit('new message', data);
+    })
+    //Output the new message
+    socket.on('new message', (data) => {
+        ('.message-container').append(`
+        <div class="message">
+            <p class="message-user">${data.sender}: </p>
+            <p class="message-text">${data.message}</p>
+        </div>
+        `);
   })
+  }
